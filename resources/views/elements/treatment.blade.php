cc8f72f4e021a5b0c5617cfa46d277dc277ec77d
Gummibeer <dev.gummibeer@gmail.com>
2019-06-20 14:11:30 +0200

    fix col sizing

--- resources/views/elements/treatment.blade.php
+++ resources/views/elements/treatment.blade.php
@@ -4,23 +4,23 @@
         @if($treatment_duration || $treatment_result || $effect_duration || $downtime)
         <div class="infobox">
             <dl class="row">
-                <div class="col-sm-6 col-lg-3">
+                <div class="col-sm-6 col-lg">
                     <dt>Behandlungsdauer</dt>
                     <dd>{{ $treatment_duration }}</dd>
                 </div>
                 @if($treatment_result)
-                    <div class="col-sm-6 col-lg-3">
+                    <div class="col-sm-6 col-lg">
                         <dt>Behandlungserfolg</dt>
                         <dd>{{ $treatment_result }}</dd>
                     </div>
                 @endif
                 @if($effect_duration)
-                <div class="col-sm-6 col-lg-3">
+                <div class="col-sm-6 col-lg">
                     <dt>Wirkungsdauer</dt>
                     <dd>{{ $effect_duration }}</dd>
                 </div>
                 @endif
-                <div class="col-sm-6 col-lg-3">
+                <div class="col-sm-6 col-lg">
                     <dt>Downtime</dt>
                     <dd>{{ $downtime ?? 'keine' }}</dd>
                 </div>
