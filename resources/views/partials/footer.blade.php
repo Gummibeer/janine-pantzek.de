5980cd9a0711c28fe55b83196a915818fabf23c3
Tom Witkowski <dev.gummibeer@gmail.com>
2020-01-05 00:45:18 +0100

    disable map until address is updated

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,4 @@
-@if(request()->is('kontakt'))
+@if(false && request()->is('kontakt'))
     <div id="footer-map">
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d759.6781376390621!2d9.98997035035257!3d53.58266977181245!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b188cc8e62e2eb%3A0x771d3aee60a28c97!2sDr.+med.+Janine+Pantzek!5e0!3m2!1sde!2sde!4v1561024074707!5m2!1sde!2sde"  width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
     </div>
